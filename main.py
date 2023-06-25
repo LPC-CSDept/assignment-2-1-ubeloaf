494e686a170961812d320cf8e56445c817588682
ubeloaf <136755601+ubeloaf@users.noreply.github.com>
2023-06-25 04:39:42 +0000

    add print statements

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     m_perc = (total_males / total_students) * 100
     f_perc = (total_females / total_students) * 100
 
+    print(f'The total number of students: \t {total_students}')
+    print(f'The number of males and females: \t {total_males} \t {total_females}')
+    print(f'The percentage of males and females: \t {m_perc:.2f}% \t {f_perc:.2f}%')
+
     return m_perc, f_perc
 
 
